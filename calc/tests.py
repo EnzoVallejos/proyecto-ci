40b874cd4c41813a7315a3e562673477dd051f5d
enzo vallejos <enzonahuelvallejos@gmail.com>
2025-06-01 11:41:20 -0300

    first commit

---
+++ calc/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.urls import reverse
+
+# Create your tests here.
+class CalculadoraViewTest(TestCase):
+    def test_suma(self):
+        response = self.client.post(reverse('calculadora'), {''
+            'num1': '5',
+            'num2': '4',
+            'operacion': 'sumar'
+            })
+        self.assertEqual(response.context['resultado'], 9)
+
+    def test_resta(self):
+        response = self.client.post(reverse('calculadora'), {''
+            'num1': '5',
+            'num2': '4',
+            'operacion': 'resta'
+            })
+        self.assertEqual(response.context['resultado'], 1)
---
+++ calc/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+# Create your views here.
+def calculadora(request):
+    resultado = None
+    num1 = int(request.POST.get('num1', 0))
+    num2 = int(request.POST.get('num2', 0))
+    operacion = request.POST.get('operacion')
+
+    if operacion == 'sumar':
+        resultado = num1 + num2
+        
+    elif operacion == 'resta':
+        resultado = num1 - num2
+
+    else:
+        resultado = "operacion invalida"
+
+    return render(request, 'base.html', {"resultado": resultado})
