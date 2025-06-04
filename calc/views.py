00805f5bda19cb88cf593832d5480a830ab6e58e
Enzo Vallejos <EnzoNahuelVallejos@gmail.com>
2025-06-04 13:13:42 -0300

    Update views.py

--- calc/views.py
+++ calc/views.py
@@ -15,6 +15,6 @@ def calculadora(request):
         resultado = num1 - num2
 
     else:
-        resultado = "operacion invalida"
+        resultado = "operacion invalidaaaa"
 
     return render(request, 'base.html', {"resultado": resultado})
